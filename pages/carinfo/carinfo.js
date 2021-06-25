d4022aca93aee39960964657c5b796780f0e0bf1
Your Name <you@example.com>
2021-06-25 17:51:36 +0800

    车辆列表-我的收集，当前审车列表-我的审车列表

---
+++ pages/carinfo/carinfo.js
@@ -0,0 +1,232 @@
+// pages/carinfo/carinfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    images: [],
+    tel: "",
+    code: "",
+    newPassword: "",
+    sendTime: '发送验证码',
+    sendColor: '#363636',
+    snsMsgWait: 60
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  
+  returnPre:function(e){
+    let pages = getCurrentPages();
+    let prevPage = pages[pages.length - 2];
+    prevPage.setData({
+      discount: e.currentTarget.dataset.discount,
+    })
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+  inputTel: function(e) {
+    this.setData({
+      tel: e.detail.value
+    })
+  },
+  inputCode: function(e) {
+    this.setData({
+      code: e.detail.value
+    })
+  },
+  inputNewpassword: function(e) {
+    this.setData({
+      newPassword: e.detail.value
+    })
+  },
+  // 获取验证码
+  sendCode: function() {
+    var that = this;
+
+    if (this.data.tel == "") {
+      this.toast('请输入手机号');
+      return;
+    }
+
+    if (!(/^1[3|4|5|8][0-9]\d{8}$/.test(this.data.tel))) {
+      this.toast('手机号输入错误');
+      return;
+    }
+
+    // 60秒后重新获取验证码
+    var inter = setInterval(function() {
+      this.setData({
+        smsFlag: true,
+        sendColor: '#cccccc',
+        sendTime: this.data.snsMsgWait + 's后重发',
+        snsMsgWait: this.data.snsMsgWait - 1
+      });
+      if (this.data.snsMsgWait < 0) {
+        clearInterval(inter)
+        this.setData({
+          sendColor: '#363636',
+          sendTime: '发送验证码',
+          snsMsgWait: 60,
+          smsFlag: false
+        });
+      }
+    }.bind(this), 1000);
+
+    // 写自己的服务器和接口- - 
+    wx.request({
+      url: ptserviceUrl + 'sendCode',
+      data: {
+        mobiles: this.data.tel,
+      },
+      method: "POST",
+      header: {
+        'content-type': "application/x-www-form-urlencoded"
+      },
+      success(res) {
+        console.log(res);
+        if (res.data.success) {
+          that.toast('短信验证码发送成功，请注意查收');
+        }
+      }
+    })
+  },
+
+  // 提交信息
+  saveClick: function() {
+    var that = this;
+    if (that.data.tel == "") {
+      that.toast("手机号不可为空");
+      return;
+    }
+    if (that.data.code == "") {
+      that.toast("验证码不可为空");
+      return;
+    }
+    if (that.data.newPassword == "") {
+      that.toast("新密码不可为空");
+      return;
+    }
+
+    var md5psd = md5Utils.hexMD5(that.data.newPassword);
+    
+    // 写自己的服务器和接口- - 
+    wx.request({
+      url: ptserviceUrl + 'forget',
+      data: {
+        mobile: this.data.tel,
+        phcode: this.data.code,
+        npwd: md5psd,
+      },
+      method: "POST",
+      header: {
+        'content-type': "application/x-www-form-urlencoded"
+      },
+      success(res) {
+        console.log(res);
+        if (res.data.success) {
+          wx.navigateBack({
+            delta: 1,
+          })
+        } else {
+          that.toast(res.data.msg);
+        }
+      }
+    })
+  },
+
+  // toast方法抽取
+  toast: function(msg) {
+    wx.showToast({
+      title: msg,
+      icon: 'none',
+      duration: 2000,
+      mask: true
+    })
+  },
+  toggleDialog() {
+    this.setData({
+      showDialog: !this.data.showDialog
+    });
+
+  },
+  two() {
+    this.setData({
+      two: !this.data.two
+    });
+
+  },
+  three() {
+    this.setData({
+      three: !this.data.three
+    });
+
+  },
+  chooseImage(e) {
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],  //可选择原图或压缩后的图片
+      sourceType: ['album', 'camera'], //可选择性开放访问相册、相机
+      success: res => {
+        const images = this.data.images.concat(res.tempFilePaths)
+        // 限制最多只能留下1张照片
+        this.data.images = images.length <= 1 ? images : images.slice(0, 3) 
+        $digest(this)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,86 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  pageLifetimes: {
+    show() {
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 0
+        })
+      }
+    }
+  },
+  data: {
+    // x轴方向的偏移
+    x: 0,
+    // 当前x的值
+    currentX: 0,
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  handleMovableChange: function(e) {
+    // this.data.currentX = e.detail.x;
+    this.data.currentX = e.detail.x;
+  },
+ 
+  handleTouchend: function(e) {
+    this.isMove = true;
+    if (this.data.currentX < -46) {
+      this.data.x = -92;
+      this.setData({
+        x: this.data.x
+      });
+    } else {
+      this.data.x = 0;
+      this.setData({
+        x: this.data.x
+      });
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
